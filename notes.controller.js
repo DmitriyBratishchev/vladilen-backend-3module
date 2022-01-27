3c73d2cea988f9bf50c1c35c4989ee304d2e34f5
DmitriyBratishchevAIM <dmitriy.bratishchev@in-aim.ru>
2022-01-27 11:20:55 +0300

    lesson2 removeNote

---
+++ notes.controller.js
@@ -0,0 +1,51 @@
+
+const fs = require('fs/promises')
+const path = require('path')
+const chalk = require('chalk')
+
+const notesPath = path.join(__dirname, 'db.json')
+// console.log(notesPath);
+
+async function addNote(title) {
+  const notes = await getNotes()
+  const note = {
+    title,
+    id: Date.now().toString()
+  }
+
+  notes.push(note)
+
+  await fs.writeFile(notesPath, JSON.stringify(notes))
+  console.log(chalk.green('Note was added!'));
+}
+
+async function getNotes() {
+  const notesString = await fs.readFile(notesPath, {encoding: 'utf-8'})
+  return Array.isArray(JSON.parse(notesString)) ? JSON.parse(notesString) : []
+}
+
+async function printNotes() {
+  const notes = await getNotes()
+  console.log(chalk.red.bgGreen('Here is the list of notes'), ':')
+  notes.forEach(note => {
+    console.log(chalk.white.bgHex('#0000ff')(note.id), chalk.blueBright(note.title));
+  });
+}
+
+async function removeNote(id) {
+  const notes = await getNotes()
+  const indexNote = notes.findIndex(n => n.id === id)
+  if (indexNote === -1) {
+    console.log(chalk.red(`Элемент с id=${id} не найден`));
+    return
+  };
+  const newNotes = notes.splice(indexNote, 1)
+  await fs.writeFile(notesPath, JSON.stringify(notes))
+  console.log('Запись', chalk.green(newNotes[0].title), 'успешно удалена.');
+}
+
+module.exports = {
+  addNote,
+  printNotes,
+  removeNote
+}
